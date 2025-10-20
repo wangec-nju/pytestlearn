a59a51832274841f0a347464b49ed9ca72772d1c
wangec-nju <251250176@smail.nju.edu.cn>
2025-10-20 23:09:19 +0800

    First version of Github CI/CD.

--- test/test_hello.py
+++ test/test_hello.py
@@ -5,7 +5,7 @@ class HelloTest:
     @pytest.mark.parametrize("name",["NOVA","NJU","CAC"])
     def test_get_name(self,name):
         instance = Hello(name)
-        assert instance.get_name() == name
+        assert instance.get_name() in ["NOVA","CAC"]
     
     @pytest.mark.parametrize("name",["NOVA","NJU","CAC"])
     def test_set_name(self,name):
